a4a61201adcd73cec0aacc7277891f27f0b07c93
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-10 16:44:55 -0700

    Imported and exported the user model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Foodtrucks = require('./foodtruckModel');
 const Geos = require('./geoModel');
+const Users = require('./userModel');
 const VeniceEvents = require('./eventModel');
 
-module.exports = { Foodtrucks, Geos, VeniceEvents };
+module.exports = { Foodtrucks, Geos, Users, VeniceEvents };
