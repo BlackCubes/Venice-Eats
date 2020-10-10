caaa534dffc14f3fc98b478684499cc53846c362
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-10 13:34:48 -0700

    Refactored

--- controllers/validationController.js
+++ controllers/validationController.js
@@ -11,52 +11,48 @@ const errMessage = errObj => {
   return message.slice(0, -1);
 };
 
-exports.createFoodtruck = catchAsync(async (req, res, next) => {
-  const validationRule = {
-    name: 'required|string|min:6|max:100|exist:Foodtrucks,name',
-    info: 'string|max:1000',
-    'contact.phoneNumber': 'string|regexPhone',
-    'contact.email': 'email',
-    'contact.website': 'url',
-    'contact.social.url1': 'url',
-    'contact.social.url2': 'url',
-    'contact.social.url3': 'url',
-    'menu.productName': 'required|string|min:2|max:100',
-    'menu.description': 'string|max:183',
-    'menu.ingredients': 'array|regexArrString',
-    'menu.price': 'required|numeric|min:1|regexPrice',
-    'menu.orderLimit': 'required|numeric|min:5',
-    'menu.availability': 'required|boolean',
-    'duration.startDateTime': 'date',
-    'duration.endDateTime': 'date'
-  };
+exports.validateFoodtruck = catchAsync(async (req, res, next) => {
+  let validationRule;
 
-  validator(req.body, validationRule, {}, (err, status) => {
-    if (!status) return next(new AppError(`${errMessage(err.errors)}`, 400));
-
-    next();
-  });
-});
-
-exports.updateFoodtruck = catchAsync(async (req, res, next) => {
-  const validationRule = {
-    name: 'string|min:6|max:100|exist:Foodtrucks,name',
-    info: 'string|max:1000',
-    'contact.phoneNumber': 'string|regexPhone',
-    'contact.email': 'email',
-    'contact.website': 'url',
-    'contact.social.url1': 'url',
-    'contact.social.url2': 'url',
-    'contact.social.url3': 'url',
-    'menu.productName': 'string|min:2|max:100',
-    'menu.description': 'string|max:183',
-    'menu.ingredients': 'array|regexArrString',
-    'menu.price': 'numeric|min:1|regexPrice',
-    'menu.orderLimit': 'numeric|min:5',
-    'menu.availability': 'boolean',
-    'duration.startDateTime': 'date',
-    'duration.endDateTime': 'date'
-  };
+  if (!req.params.id) {
+    validationRule = {
+      name: 'required|string|min:6|max:100|exist:Foodtrucks,name',
+      info: 'string|max:1000',
+      'contact.phoneNumber': 'string|regexPhone',
+      'contact.email': 'email',
+      'contact.website': 'url',
+      'contact.social.url1': 'url',
+      'contact.social.url2': 'url',
+      'contact.social.url3': 'url',
+      'menu.productName': 'required|string|min:2|max:100',
+      'menu.description': 'string|max:183',
+      'menu.ingredients': 'array|regexArrString',
+      'menu.price': 'required|numeric|min:1|regexPrice',
+      'menu.orderLimit': 'required|numeric|min:5',
+      'menu.availability': 'required|boolean',
+      'duration.startDateTime': 'date',
+      'duration.endDateTime': 'date'
+    };
+  } else {
+    validationRule = {
+      name: 'string|min:6|max:100|exist:Foodtrucks,name',
+      info: 'string|max:1000',
+      'contact.phoneNumber': 'string|regexPhone',
+      'contact.email': 'email',
+      'contact.website': 'url',
+      'contact.social.url1': 'url',
+      'contact.social.url2': 'url',
+      'contact.social.url3': 'url',
+      'menu.productName': 'string|min:2|max:100',
+      'menu.description': 'string|max:183',
+      'menu.ingredients': 'array|regexArrString',
+      'menu.price': 'numeric|min:1|regexPrice',
+      'menu.orderLimit': 'numeric|min:5',
+      'menu.availability': 'boolean',
+      'duration.startDateTime': 'date',
+      'duration.endDateTime': 'date'
+    };
+  }
 
   validator(req.body, validationRule, {}, (err, status) => {
     if (!status) return next(new AppError(`${errMessage(err.errors)}`, 400));
