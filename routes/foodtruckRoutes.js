254deee56848f710a9014c1155e865a5ac9c2ca4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-10 00:22:18 -0700

    Created an express router and fixed syntax error

--- routes/foodtruckRoutes.js
+++ routes/foodtruckRoutes.js
@@ -1,4 +1,7 @@
+const express = require('express');
 const foodtruckController = require('./../controllers/foodtruckController');
+
+const router = express.Router();
 
 router.route('/').post(foodtruckController.createFoodtruck);
 
