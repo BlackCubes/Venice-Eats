cc1d352b52269deef9ceae50720e39678eb62f0e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-10 16:20:38 -0700

    Imported the validation middleware, and routed it to the patch and post routes

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const eventController = require('./../controllers/eventController');
+const validationController = require('./../controllers/validationController');
 
 const router = express.Router();
 
 router
   .route('/')
   .get(eventController.getAllVeniceEvents)
-  .post(eventController.createVeniceEvent);
+  .post(validationController.validateEvent, eventController.createVeniceEvent);
 
 router
   .route('/:id')
   .get(eventController.getVeniceEvent)
-  .patch(eventController.updateVeniceEvent)
+  .patch(validationController.validateEvent, eventController.updateVeniceEvent)
   .delete(eventController.deleteVeniceEvent);
 
 module.exports = router;
