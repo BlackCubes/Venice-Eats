32d6950ca2c82e677ae613748ea44dcfb96a1a47
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-10 01:11:28 -0700

    Created the rest of the handler factories for CRUD operation

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -13,3 +13,66 @@ exports.createOne = Model =>
       }
     });
   });
+
+exports.getOne = (Model, ...popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Models[Model].findById(req.params.id);
+
+    if (popOptions) query = query.populate(popOptions);
+
+    const doc = await query;
+
+    if (!doc)
+      return next(new AppError('There is no document with that ID!', 404));
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
+
+exports.getAll = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Models[Model].find();
+
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        data: doc
+      }
+    });
+  });
+
+exports.updateOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Models[Model].findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!doc)
+      return next(new AppError('There is no document with that ID!', 404));
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
+
+exports.deleteOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Models[Model].findByIdAndDelete(req.params.id);
+
+    if (!doc)
+      return next(new AppError('There is no document with that ID!', 404));
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  });
