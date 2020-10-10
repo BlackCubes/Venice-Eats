c46d89d41e2ed20258c35481ce0ea0b526213da0
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-10 01:16:18 -0700

    Create the rest of the CRUD foodtruck middlewares

--- controllers/foodtruckController.js
+++ controllers/foodtruckController.js
@@ -1,3 +1,7 @@
 const factory = require('./handlerFactory');
 
 exports.createFoodtruck = factory.createOne('Foodtrucks');
+exports.getFoodtruck = factory.getOne('Foodtrucks', 'veniceevents');
+exports.getAllFoodtrucks = factory.getAll('Foodtrucks');
+exports.updateFoodtruck = factory.updateOne('Foodtrucks');
+exports.deleteFoodtruck = factory.deleteOne('Foodtrucks');
