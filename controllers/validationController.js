0cc6d868dc40554b87563160a0c91d123f29fd09
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-10 12:56:55 -0700

    Created a validation for creating a food truck and exported it

--- controllers/validationController.js
+++ controllers/validationController.js
@@ -10,3 +10,30 @@ const errMessage = errObj => {
 
   return message.slice(0, -1);
 };
+
+exports.createFoodtruck = catchAsync(async (req, res, next) => {
+  const validationRule = {
+    name: 'required|string|min:6|max:100|exist:Foodtrucks,name',
+    info: 'string|max:1000',
+    'contact.phoneNumber': 'string|regexPhone',
+    'contact.email': 'email',
+    'contact.website': 'url',
+    'contact.social.url1': 'url',
+    'contact.social.url2': 'url',
+    'contact.social.url3': 'url',
+    'menu.productName': 'required|string|min:2|max:100',
+    'menu.description': 'string|max:183',
+    'menu.ingredients': 'array|regexArrString',
+    'menu.price': 'required|numeric|min:1|regexPrice',
+    'menu.orderLimit': 'required|numeric|min:5',
+    'menu.availability': 'required|boolean',
+    'duration.startDateTime': 'date',
+    'duration.endDateTime': 'date'
+  };
+
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) return next(new AppError(`${errMessage(err.errors)}`, 400));
+
+    next();
+  });
+});
