88780cfa185243505129a930ce29e992eb68b707
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-10 13:31:04 -0700

    Created a validation middleware for updating Foodtrucks

--- controllers/validationController.js
+++ controllers/validationController.js
@@ -37,3 +37,30 @@ exports.createFoodtruck = catchAsync(async (req, res, next) => {
     next();
   });
 });
+
+exports.updateFoodtruck = catchAsync(async (req, res, next) => {
+  const validationRule = {
+    name: 'string|min:6|max:100|exist:Foodtrucks,name',
+    info: 'string|max:1000',
+    'contact.phoneNumber': 'string|regexPhone',
+    'contact.email': 'email',
+    'contact.website': 'url',
+    'contact.social.url1': 'url',
+    'contact.social.url2': 'url',
+    'contact.social.url3': 'url',
+    'menu.productName': 'string|min:2|max:100',
+    'menu.description': 'string|max:183',
+    'menu.ingredients': 'array|regexArrString',
+    'menu.price': 'numeric|min:1|regexPrice',
+    'menu.orderLimit': 'numeric|min:5',
+    'menu.availability': 'boolean',
+    'duration.startDateTime': 'date',
+    'duration.endDateTime': 'date'
+  };
+
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) return next(new AppError(`${errMessage(err.errors)}`, 400));
+
+    next();
+  });
+});
