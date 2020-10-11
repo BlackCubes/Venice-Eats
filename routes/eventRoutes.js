e0318aa87d6feda2e4e9eca099c0b153a78b6672
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-11 10:33:42 -0700

    Added auth middlewares into routes

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const authController = require('./../controllers/authController');
 const eventController = require('./../controllers/eventController');
 const validationController = require('./../controllers/validationController');
 
@@ -7,12 +8,26 @@ const router = express.Router();
 router
   .route('/')
   .get(eventController.getAllVeniceEvents)
-  .post(validationController.validateEvent, eventController.createVeniceEvent);
+  .post(
+    authController.protect,
+    authController.restrictTo('admin'),
+    validationController.validateEvent,
+    eventController.createVeniceEvent
+  );
 
 router
   .route('/:id')
   .get(eventController.getVeniceEvent)
-  .patch(validationController.validateEvent, eventController.updateVeniceEvent)
-  .delete(eventController.deleteVeniceEvent);
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin'),
+    validationController.validateEvent,
+    eventController.updateVeniceEvent
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin'),
+    eventController.deleteVeniceEvent
+  );
 
 module.exports = router;
