6a69e6e98f0052bcf6c3b53283b03f43049ac204
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-10 17:04:21 -0700

    Created Document middlewares, Instance middlewares, and Static methods onto user model

--- models/userModel.js
+++ models/userModel.js
@@ -49,6 +49,67 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+// DOCUMENT MIDDLEWARES
+// -- salt password
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
+});
+
+// -- update the date is password is changed
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+// INSTANCE METHODS
+// -- check for correct password
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+// -- check for changed password after token issued
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+  return false;
+};
+
+// -- create user password reset
+userSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
+// STATIC METHODS
+// -- find query in DB
+userSchema.statics.valueExists = function(query) {
+  return this.findOne(query).then(result => result);
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
