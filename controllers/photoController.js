990b02f074c05841ce8cbd5b3849daa66968d200
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-12 08:33:21 -0700

    Debugging

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -72,6 +72,7 @@ exports.uploadPhoto = (preset, required = true) =>
     if (!foodtruckPhoto && !required) return next();
 
     const file64 = formatBufferTo64(foodtruckPhoto[0]);
+    console.log('Foodtruck photo: ', foodtruckPhoto);
     console.log('File64: ', file64);
 
     // const cloudinaryResult = await cloudinaryUpload(file64.content, preset);
