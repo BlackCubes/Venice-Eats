b1f94be44daa9fcc141b753346ae0b8ff5568a66
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-19 21:25:06 -0700

    Added test scripts

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-function App() {
-  return <div className="App">Hello! This is a test for Venice Eats!</div>;
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {apiResponse: ''};
+  }
+
+  callApi() {
+    fetch('http://localhost:3001/testApi')
+      .then(res => res.text())
+      .then(res => this.setState({apiResponse: res}));
+  }
+
+  componentWillMount() {
+    this.callApi();
+  }
+}
+
+render() {
+  return (
+    <div className="App">
+      Hello! This is a test for Venice Eats!
+      <p>{this.state.apiResponse}</p>
+    </div>
+  );
 }
 
 export default App;
