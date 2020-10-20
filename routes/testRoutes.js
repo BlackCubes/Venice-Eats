7562124e7dc690ee3fcd0d4dff04062ae0fc37f1
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-19 21:05:44 -0700

    Created a test router

---
+++ routes/testRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    apiTest: 'API test is working from Node to React! 💯'
+  });
+});
+
+module.exports = router;
