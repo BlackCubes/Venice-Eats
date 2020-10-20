c796cb66141e1032677a0d0cd574a609081cc0c4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-10-19 21:26:11 -0700

    Fixed syntax error

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,28 @@
 import React from 'react';
 
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {apiResponse: ''};
+    this.state = { apiResponse: '' };
   }
 
   callApi() {
     fetch('http://localhost:3001/testApi')
       .then(res => res.text())
-      .then(res => this.setState({apiResponse: res}));
+      .then(res => this.setState({ apiResponse: res }));
   }
 
   componentWillMount() {
     this.callApi();
   }
-}
 
-render() {
-  return (
-    <div className="App">
-      Hello! This is a test for Venice Eats!
-      <p>{this.state.apiResponse}</p>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        Hello! This is a test for Venice Eats!
+        <p>{this.state.apiResponse}</p>
+      </div>
+    );
+  }
 }
-
 export default App;
