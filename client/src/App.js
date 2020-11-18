b039bce09eb0ecefe58a60ef4b79a6ca36fe981d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-18 07:05:11 -0800

    Refactored

--- client/src/App.js
+++ client/src/App.js
@@ -38,30 +38,3 @@ class App extends Component {
 }
 
 export default App;
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { apiResponse: '' };
-//   }
-
-//   callApi() {
-//     fetch('http://localhost:3001/testApi')
-//       .then(res => res.json())
-//       .then(data => this.setState({ apiResponse: data.apiTest }));
-//   }
-
-//   componentWillMount() {
-//     this.callApi();
-//   }
-
-//   render() {
-//     return (
-//       <div className="App">
-//         Hello! This is a test for Venice Eats!
-//         <p>{this.state.apiResponse}</p>
-//       </div>
-//     );
-//   }
-// }
-// export default App;
