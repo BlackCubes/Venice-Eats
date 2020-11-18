cebc4a4df814f6e5009e4337d360493fb61ceb4c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-17 18:46:39 -0800

    Changed the route url

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/testApi', (req, res, next) => {
   res.status(200).json({
     status: 'success',
     apiTest: 'API test is working from Node to React! 💯'
