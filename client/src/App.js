ee0e66c8fa846a66202e6a0929764c73b2fec325
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-17 19:12:39 -0800

    Refactoring for axios api calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import axios from 'axios';
+// import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+const apiBaseUrl = axios.create({
+  baseURL: 'http://localhost:3001/api',
+  responseType: 'json'
+});
 
 class App extends Component {
   constructor(props) {
@@ -6,23 +13,51 @@ class App extends Component {
     this.state = { apiResponse: '' };
   }
 
-  callApi() {
-    fetch('http://localhost:3001/testApi')
-      .then(res => res.json())
-      .then(data => this.setState({ apiResponse: data.apiTest }));
-  }
+  async componentWillMount() {
+    try {
+      const data = await apiBaseUrl.get('/testApi');
 
-  componentWillMount() {
-    this.callApi();
+      this.setState({ apiResponse: data.apiTest });
+    } catch (err) {
+      console.log(`There was an error fetching the api: ${err.message}`);
+    }
   }
 
   render() {
     return (
       <div className="App">
-        Hello! This is a test for Venice Eats!
+        Hello! This is a new test for Venice Eats!
         <p>{this.state.apiResponse}</p>
       </div>
     );
   }
 }
+
 export default App;
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { apiResponse: '' };
+//   }
+
+//   callApi() {
+//     fetch('http://localhost:3001/testApi')
+//       .then(res => res.json())
+//       .then(data => this.setState({ apiResponse: data.apiTest }));
+//   }
+
+//   componentWillMount() {
+//     this.callApi();
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         Hello! This is a test for Venice Eats!
+//         <p>{this.state.apiResponse}</p>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
