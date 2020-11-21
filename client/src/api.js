b5b682d5d0f8e3db2e75aa301de58a5996bb875d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-21 15:49:53 -0800

    Created a simple test api with axios

---
+++ client/src/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { resolve } from 'utils/resolve';
+
+const apiUrl = axios.create({
+  baseURL: 'http://localhost:3001/api/v1',
+  responseType: 'json'
+});
+
+export async function getTest() {
+  return await resolve(axios.get(apiUrl).then(res => res.data));
+}
