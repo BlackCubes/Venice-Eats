9c07db20982ab08a8310f06406313675d75b3023
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-22 11:04:16 -0800

    Created react routers

---
+++ client/src/AppRoutes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { Login } from './containers/Login';
+import { Test } from './containers/Test';
+
+const AppTesting = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Test} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppTesting;
