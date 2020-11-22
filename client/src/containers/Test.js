7aa3cfc62468e15496b46594cbe439381efa6006
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-22 11:03:33 -0800

    Created a test for the homepage and api

---
+++ client/src/containers/Test.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { getTest } from './../services/api';
+
+export default class Test extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { res: '' };
+  }
+
+  callApi = async () => {
+    const testApi = await getTest();
+
+    if (testApi.error) this.setState({ res: testApi.error });
+    else this.setState({ res: testApi.data });
+  };
+
+  componentDidMount() {
+    this.callApi();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello! This is just a test route for the home page!
+        <p>{this.state.res}</p>
+      </div>
+    );
+  }
+}
