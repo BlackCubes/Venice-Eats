457bb8f4411ae38d8456d97727a73c0d29274853
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-21 16:19:00 -0800

    Debugging

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
       <div className="App">
         Hello! This is a new test for Venice Eats!
         <p>{this.state.apiResponse}</p>
-        <p>{this.state.error}</p>
+        {/* <p>{this.state.error}</p> */}
       </div>
     );
   }
