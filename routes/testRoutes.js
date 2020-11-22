e749ab19d22874758170ef7f045bb30c0f1fb67b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-21 16:37:48 -0800

    Debugging

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.status(200).json({
     status: 'success',
-    apiTest: 'API test is working from Node to React on /testApi! 💯'
+    data: 'API test is working from Node to React on /testApi! 💯'
   });
 });
 
