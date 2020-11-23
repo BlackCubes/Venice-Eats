5cd25c0b1ee1a86914f4982f52c82423bfc2ded2
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-23 07:39:03 -0800

    Imported the css

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -3,6 +3,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { login } from './../services/authentication';
 import { Alert } from './../components/Alert';
+import './Login.css';
 
 export class Login extends Component {
   constructor(props) {
