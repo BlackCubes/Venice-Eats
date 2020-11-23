177e9248b39b56a20c7c1c2183135ca10916f0ac
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-22 19:22:37 -0800

    Added alert message inside the form container

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -34,33 +34,37 @@ export class Login extends Component {
   }
 
   render() {
-    if (this.state.error) {
-      return <Alert message={this.state.error} />;
-    }
+    // if (this.state.error) {
+    //   return <Alert message={this.state.error} />;
+    // }
 
     return (
-      <Form onSubmit={this.handleLoginForm}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={this.state.email}
-            onChange={e => this.handleChange({ email: e.target.value })}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={this.state.password}
-            onChange={e => this.handleChange({ password: e.target.value })}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit">
-          Login
-        </Button>
-      </Form>
+      <div className="form-container">
+        {this.state.error && <Alert message={this.state.error} />}
+
+        <Form onSubmit={this.handleLoginForm}>
+          <Form.Group size="lg" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={e => this.handleChange({ email: e.target.value })}
+            />
+          </Form.Group>
+          <Form.Group size="lg" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={this.state.password}
+              onChange={e => this.handleChange({ password: e.target.value })}
+            />
+          </Form.Group>
+          <Button block size="lg" type="submit">
+            Login
+          </Button>
+        </Form>
+      </div>
     );
   }
 }
