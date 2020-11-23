e953287e087f75e56c9fe471c5cd24484df173ac
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-22 18:32:47 -0800

    Properly used the react way

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -1,8 +1,10 @@
 import { Alert as AlertBS } from 'react-bootstrap/Alert';
 
 export const Alert = message => {
-  <AlertBS variant="danger">
-    <AlertBS.Heading>Oh no! 😱 You got errors! 🙅‍♀️</AlertBS.Heading>
-    <p>{message}</p>
-  </AlertBS>;
+  return (
+    <AlertBS variant="danger">
+      <AlertBS.Heading>Oh no! 😱 You got errors! 🙅‍♀️</AlertBS.Heading>
+      <p>{message}</p>
+    </AlertBS>
+  );
 };
