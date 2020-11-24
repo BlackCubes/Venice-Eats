3b5242bab64c1720736c8771a8c848a74fa5ce35
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-23 18:02:34 -0800

    Fixed syntax error

--- client/src/AppRoutes.js
+++ client/src/AppRoutes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import { Login } from './containers/Login';
+import Login from './containers/Login';
 import { Test } from './containers/Test';
 
 const App = () => {
