c0e7724357968dea35be12eca35a5a55d22f3b5d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-24 15:46:10 -0800

    Imported the admin page container and routed it

--- client/src/AppRoutes.js
+++ client/src/AppRoutes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import AdminPage from './containers/AdminPage';
 import LoginPage from './containers/LoginPage';
 import { Test } from './containers/Test';
 
@@ -9,6 +10,7 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={Test} />
         <Route path="/login" exact component={LoginPage} />
+        <Route path="/admin" exact component={AdminPage} />
       </Switch>
     </Router>
   );
