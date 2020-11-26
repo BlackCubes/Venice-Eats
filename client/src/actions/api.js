dd13cfa15b348fc152b45076fd9644c9442f4b8b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-25 20:24:52 -0800

    Added two more action types and creators

--- client/src/actions/api.js
+++ client/src/actions/api.js
@@ -1,5 +1,7 @@
 // ACTION TYPES
 export const API_REQUEST = 'API_REQUEST';
+export const API_SUCCESS = 'API_SUCCESS';
+export const API_ERROR = 'API_ERROR';
 
 // ACTION CREATORS
 export const apiRequest = ({ url, method, data }) => {
@@ -8,3 +10,13 @@ export const apiRequest = ({ url, method, data }) => {
     meta: { url, method, data }
   };
 };
+
+export const apiSuccess = ({ response }) => ({
+  type: API_SUCCESS,
+  payload: response
+});
+
+export const apiError = ({ error }) => ({
+  type: API_ERROR,
+  payload: error
+});
