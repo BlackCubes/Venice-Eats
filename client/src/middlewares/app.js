bfbc8bf43ae7e051fc899885b47fccca234e14f6
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-25 20:07:54 -0800

    Created an app middleware to handle API requests

---
+++ client/src/middlewares/app.js
@@ -0,0 +1,20 @@
+import { apiRequest } from './../actions/api';
+import { LOGIN } from './../actions/auth';
+
+export const appMiddleware = () => next => action => {
+  next(action);
+  switch (action.type) {
+    case LOGIN: {
+      next(
+        apiRequest({
+          url: `${process.env.SERVER_URL}/admins/login`,
+          method: 'POST',
+          data: action.payload
+        })
+      );
+      break;
+    }
+    default:
+      break;
+  }
+};
