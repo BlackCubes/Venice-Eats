69f6a1364b7061fd86c5db3ea3e56f8782309151
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-26 10:49:19 -0800

    Changed the redirect route

--- client/src/components/AuthRoute.js
+++ client/src/components/AuthRoute.js
@@ -5,7 +5,7 @@ import { Redirect, Route } from 'react-router-dom';
 const AuthRoute = props => {
   const { isAuthUser, type } = props;
   if (type === 'guest' && isAuthUser) return <Redirect to="/" />;
-  else if (type === 'private' && !isAuthUser) return <Redirect to="/admin" />;
+  else if (type === 'private' && !isAuthUser) return <Redirect to="/login" />;
 
   return <Route {...props} />;
 };
