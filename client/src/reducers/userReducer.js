b3f664c3c5549fb3781175f8930e8db92679a40f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-27 22:35:57 -0800

    Created a user reducer function

---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,25 @@
+import { SET_LOADER } from './../actions/ui';
+import { API_SUCCESS, API_ERROR } from './../actions/api';
+
+export default (
+  state = {
+    data: null,
+    isLoading: false,
+    error: null
+  },
+  action
+) => {
+  switch (action.type) {
+    case API_SUCCESS:
+      return {
+        ...state,
+        data: action.payload.data.data
+      };
+    case API_ERROR:
+      return { ...state, error: action.payload };
+    case SET_LOADER:
+      return { ...state, isLoading: action.payload };
+    default:
+      return state;
+  }
+};
