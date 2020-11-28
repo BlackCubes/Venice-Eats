01167bc33a657d15eed1488148aae36438ef715f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-27 21:52:52 -0800

    Refactored with a switch conditionals, and added a get all network request to the api

--- client/src/middlewares/core.js
+++ client/src/middlewares/core.js
@@ -1,22 +1,59 @@
 import axios from 'axios';
-import { API_POST_REQUEST, apiSuccess, apiError } from './../actions/api';
+import {
+  API_POST_REQUEST,
+  API_GET_ALL_REQUEST,
+  apiSuccess,
+  apiError
+} from './../actions/api';
 import { setLoader } from './../actions/ui';
 
 export const apiMiddleware = ({ dispatch }) => next => action => {
   next(action);
-
-  if (action.type === API_POST_REQUEST) {
-    dispatch(setLoader(true));
-    const { url, method, data } = action.meta;
-    axios({
-      method,
-      url,
-      data
-    })
-      .then(({ data }) => dispatch(apiSuccess({ response: data })))
-      .catch(error => {
-        console.log(error);
-        dispatch(apiError({ error: error.response.data }));
-      });
+  switch (action.type) {
+    case API_POST_REQUEST: {
+      dispatch(setLoader(true));
+      const { url, method, data } = action.meta;
+      axios({
+        method,
+        url,
+        data
+      })
+        .then(({ data }) => dispatch(apiSuccess({ response: data })))
+        .catch(error => {
+          console.log(error);
+          dispatch(apiError({ error: error.response.data }));
+        });
+      break;
+    }
+    case API_GET_ALL_REQUEST: {
+      dispatch(setLoader(true));
+      const { url, method } = action.meta;
+      axios({
+        method,
+        url
+      })
+        .then(({ data }) => dispatch(apiSuccess({ response: data })))
+        .catch(error => {
+          console.log(error);
+          dispatch(apiError({ error: error.response.data }));
+        });
+      break;
+    }
+    default:
+      break;
   }
+  // if (action.type === API_POST_REQUEST) {
+  //   dispatch(setLoader(true));
+  //   const { url, method, data } = action.meta;
+  //   axios({
+  //     method,
+  //     url,
+  //     data
+  //   })
+  //     .then(({ data }) => dispatch(apiSuccess({ response: data })))
+  //     .catch(error => {
+  //       console.log(error);
+  //       dispatch(apiError({ error: error.response.data }));
+  //     });
+  // }
 };
