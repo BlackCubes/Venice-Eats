b13919727b5426a539343c25264a2425f791b421
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-27 21:36:23 -0800

    Handled the get all users api request, and created api route constants

--- client/src/middlewares/app.js
+++ client/src/middlewares/app.js
@@ -1,5 +1,11 @@
-import { apiPostRequest } from './../actions/api';
+import { apiPostRequest, apiGetAllRequest } from './../actions/api';
 import { LOGIN } from './../actions/auth';
+import { GET_USERS } from './../actions/user';
+
+const FOODTRUCK_ROUTE = 'foodtrucks';
+const GEO_ROUTE = 'geos';
+const USER_ROUTE = 'users';
+const VENICEEVENT_ROUTE = 'veniceevents';
 
 export const appMiddleware = () => next => action => {
   next(action);
@@ -13,6 +19,14 @@ export const appMiddleware = () => next => action => {
         })
       );
       break;
+    }
+    case GET_USERS: {
+      next(
+        apiGetAllRequest({
+          url: `${process.env.REACT_APP_SERVER_URL}/${USER_ROUTE}/`,
+          method: 'GET'
+        })
+      );
     }
     default:
       break;
