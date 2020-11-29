ca0d0e4088e6c0e2373c452a6ee5095eba02aad5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-29 14:29:19 -0800

    Created middleware to update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const factory = require('./handlerFactory');
 exports.createUser = factory.createOne('Users');
 exports.getAllUsers = factory.getAll('Users');
 exports.getUser = factory.getOne('Users');
+exports.updateUser = factory.updateOne('Users');
 exports.deleteUser = factory.deleteOne('Users');
 
 exports.getMe = (req, res, next) => {
