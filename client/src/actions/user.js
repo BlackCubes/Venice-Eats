a56aadbdbb47d4d3ab8f92026c29f7ece851d93a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-28 16:06:26 -0800

    Created a post user action type and creator

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,7 +1,15 @@
 // ACTION TYPES
+export const POST_USER = 'POST_USER';
 export const GET_USERS = 'GET_USERS';
 
 // ACTION CREATORS
+export const postUser = data => {
+  return {
+    type: POST_USER,
+    payload: data
+  };
+};
+
 export const getUsers = () => {
   return {
     type: GET_USERS
