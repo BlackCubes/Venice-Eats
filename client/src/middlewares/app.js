c75f17ec0d274d74ef4ba260ed36b0c878efef4a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-29 12:14:32 -0800

    Added a case of updating user for api request middleware

--- client/src/middlewares/app.js
+++ client/src/middlewares/app.js
@@ -3,10 +3,17 @@ import {
   apiPostRequest,
   apiGetAllRequest,
   apiGetRequest,
+  apiUpdateRequest,
   apiDeleteRequest
 } from './../actions/api';
 import { LOGIN, tokenHeadersConfig } from './../actions/auth';
-import { POST_USER, GET_USERS, GET_USER, DELETE_USER } from './../actions/user';
+import {
+  POST_USER,
+  GET_USERS,
+  GET_USER,
+  UPDATE_USER,
+  DELETE_USER
+} from './../actions/user';
 
 const FOODTRUCK_ROUTE = 'foodtrucks';
 const GEO_ROUTE = 'geos';
@@ -51,6 +58,17 @@ export const appMiddleware = ({ getState }) => next => action => {
         apiGetRequest({
           url: `${process.env.REACT_APP_SERVER_URL}/${USER_ROUTE}/${action.payload}`,
           method: 'GET',
+          headers: tokenHeadersConfig(getState)
+        })
+      );
+      break;
+    }
+    case UPDATE_USER: {
+      next(
+        apiUpdateRequest({
+          url: `${process.env.REACT_APP_SERVER_URL}/${USER_ROUTE}/${action.payload.id}`,
+          method: 'PATCH',
+          data: action.payload.data,
           headers: tokenHeadersConfig(getState)
         })
       );
