4daaf1d9885f4efd5318e8609ab3786af943c2bc
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-29 11:49:54 -0800

    Changed the action type variable name to reflect the api request

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -2,7 +2,7 @@
 export const POST_USER = 'POST_USER';
 export const GET_USERS = 'GET_USERS';
 export const GET_USER = 'GET_USER';
-export const UPDATE_USER = 'UPDATE_USER';
+export const PATCH_USER = 'PATCH_USER';
 export const DELETE_USER = 'DELETE_USER';
 
 // ACTION CREATORS
@@ -28,7 +28,7 @@ export const getUser = params => {
 
 export const updateUser = (id, data) => {
   return {
-    type: UPDATE_USER,
+    type: PATCH_USER,
     payload: { id, data }
   };
 };
