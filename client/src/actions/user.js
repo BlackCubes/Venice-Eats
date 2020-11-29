07e90476823946dcb2e8b7f9297b5594d9c91975
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-29 11:41:58 -0800

    Added an update user action type and creator

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -2,6 +2,7 @@
 export const POST_USER = 'POST_USER';
 export const GET_USERS = 'GET_USERS';
 export const GET_USER = 'GET_USER';
+export const UPDATE_USER = 'UPDATE_USER';
 export const DELETE_USER = 'DELETE_USER';
 
 // ACTION CREATORS
@@ -22,6 +23,13 @@ export const getUser = params => {
   return {
     type: GET_USER,
     payload: params
+  };
+};
+
+export const updateUser = id => {
+  return {
+    type: UPDATE_USER,
+    payload: id
   };
 };
 
