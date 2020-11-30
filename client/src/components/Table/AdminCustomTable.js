c37256c253356d0e10fbe3905b5b6157226f9604
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-30 13:00:59 -0800

    Created one function to output the table header along with variables

--- client/src/components/Table/AdminCustomTable.js
+++ client/src/components/Table/AdminCustomTable.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { Table, Row, Col, Button, Spinner } from 'react-bootstrap';
 
 export default props => {
+  const { thValues, apiData, loadingApi } = props;
+
+  const thOutput = thValues => {
+    const th = [];
+    for (var i = 0; i < thValues.length; i++) {
+      th.push(<th key={i}>{thValues[i]}</th>);
+    }
+    return th;
+  };
+
   return (
     <React.Fragment>
       <Row>
         <Col md={12}>
           <Table striped hover>
             <thead>
-              <tr></tr>
+              <tr>{thOutput(thValues)}</tr>
             </thead>
             <tbody></tbody>
           </Table>
