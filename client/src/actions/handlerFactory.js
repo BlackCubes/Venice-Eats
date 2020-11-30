aa7d9c56f692c60509217875934c2a30dd61587a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-30 08:34:24 -0800

    Created a boilerplate template for the handling redux requests

---
+++ client/src/actions/handlerFactory.js
@@ -0,0 +1,42 @@
+// ACTION TYPES
+export const POST_ONE = 'POST_ONE';
+export const GET_ALL = 'GET_ALL';
+export const GET_ONE = 'GET_ONE';
+export const UPDATE_ONE = 'UPDATE_ONE';
+export const DELETE_ONE = 'DELETE_ONE';
+
+// ACTION CREATORS
+export const postOne = (path, data) => {
+  return {
+    type: POST_ONE,
+    payload: { path, data }
+  };
+};
+
+export const getAll = path => {
+  return {
+    type: GET_ALL,
+    payload: { path }
+  };
+};
+
+export const getOne = (path, params) => {
+  return {
+    type: GET_ONE,
+    payload: { path, params }
+  };
+};
+
+export const updateOne = (path, params, data) => {
+  return {
+    type: UPDATE_ONE,
+    payload: { path, params, data }
+  };
+};
+
+export const deleteOne = (path, id) => {
+  return {
+    type: DELETE_ONE,
+    payload: { path, id }
+  };
+};
