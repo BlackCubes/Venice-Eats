969d882518358da3a22df37498af008fdb6dfd8c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-11-30 19:31:33 -0800

    Brought back the old code for debugging purposes

--- client/src/containers/AdminPage.js
+++ client/src/containers/AdminPage.js
@@ -20,12 +20,121 @@ const AdminPage = ({
   const handleDelete = id => deleteOne('admin', id);
 
   return (
-    <AdminCustomTable
-      thValues={['ID', 'Name', 'Email', 'Role']}
-      trValues={['_id', 'name', 'email', 'role']}
-      loadingApi={loadingUserApi}
-      apiDatas={apiDatas}
-    />
+    <div>
+      <Row>
+        <Col md={12}>
+          <AdminCustomTable
+            thValues={['ID', 'Name', 'Email', 'Role']}
+            trValues={['_id', 'name', 'email', 'role']}
+            loadingApi={loadingUserApi}
+            apiDatas={apiDatas}
+          />
+          {/* <Table striped hover>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Delete</th>
+                <th>View</th>
+              </tr>
+            </thead>
+            <tbody>
+              {!loadingUserApi ? (
+                apiDatas.map(prop => (
+                  <tr key={prop._id}>
+                    <td>{prop._id}</td>
+                    <td>{prop.name}</td>
+                    <td>{prop.email}</td>
+                    <td>{prop.role}</td>
+                    <td>
+                      <Button
+                        variant="danger"
+                        size="sm"
+                        disabled={loadingUserApi}
+                        onClick={() => handleDelete(prop._id)}
+                      >
+                        {loadingUserApi ? (
+                          <Spinner
+                            as="span"
+                            animation="grow"
+                            size="sm"
+                            role="status"
+                          />
+                        ) : (
+                          'DEL'
+                        )}
+                      </Button>
+                    </td>
+                    <td>
+                      <Button
+                        variant="info"
+                        href={`/admin/users/${prop._id}`}
+                        type="button"
+                      >
+                        View
+                      </Button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td>
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                    />
+                  </td>
+                  <td>
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                    />
+                  </td>
+                  <td>
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                    />
+                  </td>
+                  <td>
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                    />
+                  </td>
+                  <td>
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                    />
+                  </td>
+                  <td>
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                    />
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </Table> */}
+        </Col>
+      </Row>
+    </div>
   );
 };
 
