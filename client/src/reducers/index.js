f8b75ab782c9e586896fc794cff7f5eb3dc021ad
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-12-04 12:15:05 -0800

    Imported and implemented the error reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import authReducer from './authReducer';
+import errorReducer from './errorReducer';
 import userReducer from './userReducer';
 
 export default combineReducers({
   auth: authReducer,
-  apiUser: userReducer
+  apiUser: userReducer,
+  apiError: errorReducer
 });
