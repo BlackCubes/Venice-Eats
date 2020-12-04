f971967e2e12a2f64fe596a85e9de5ebd29339fb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-12-03 17:54:37 -0800

    Refactored

--- client/src/containers/AdminPage.js
+++ client/src/containers/AdminPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Table, Row, Col, Button, Spinner } from 'react-bootstrap';
 
 import { getAll, deleteOne } from './../actions/handlerFactory';
 
@@ -29,109 +28,6 @@ const AdminPage = ({
         loadingApi={loadingUserApi}
         apiDatas={apiDatas}
       />
-      {/* <Table striped hover>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>Delete</th>
-            <th>View</th>
-          </tr>
-        </thead>
-        <tbody>
-          {!loadingUserApi ? (
-            apiDatas.map(prop => (
-              <tr key={prop._id}>
-                <td>{prop._id}</td>
-                <td>{prop.name}</td>
-                <td>{prop.email}</td>
-                <td>{prop.role}</td>
-                <td>
-                  <Button
-                    variant="danger"
-                    size="sm"
-                    disabled={loadingUserApi}
-                    onClick={() => handleDelete(prop._id)}
-                  >
-                    {loadingUserApi ? (
-                      <Spinner
-                        as="span"
-                        animation="grow"
-                        size="sm"
-                        role="status"
-                      />
-                    ) : (
-                      'DEL'
-                    )}
-                  </Button>
-                </td>
-                <td>
-                  <Button
-                    variant="info"
-                    href={`/admin/users/${prop._id}`}
-                    type="button"
-                  >
-                    View
-                  </Button>
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td>
-                <Spinner
-                  as="span"
-                  animation="border"
-                  size="sm"
-                  role="status"
-                />
-              </td>
-              <td>
-                <Spinner
-                  as="span"
-                  animation="border"
-                  size="sm"
-                  role="status"
-                />
-              </td>
-              <td>
-                <Spinner
-                  as="span"
-                  animation="border"
-                  size="sm"
-                  role="status"
-                />
-              </td>
-              <td>
-                <Spinner
-                  as="span"
-                  animation="border"
-                  size="sm"
-                  role="status"
-                />
-              </td>
-              <td>
-                <Spinner
-                  as="span"
-                  animation="border"
-                  size="sm"
-                  role="status"
-                />
-              </td>
-              <td>
-                <Spinner
-                  as="span"
-                  animation="border"
-                  size="sm"
-                  role="status"
-                />
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </Table> */}
     </div>
   );
 };
