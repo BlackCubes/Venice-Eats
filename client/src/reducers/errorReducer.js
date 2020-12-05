5bedc7c320f24ffcfd2e21d8be8c006d89c7201b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-12-04 20:37:33 -0800

    Added a case of api success for error reducer

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,7 +1,9 @@
-import { API_ERROR } from './../actions/api';
+import { API_SUCCESS, API_ERROR } from './../actions/api';
 
 export default (state = { error: null }, action) => {
   switch (action.type) {
+    case API_SUCCESS:
+      return { ...state, error: null };
     case API_ERROR:
       return { ...state, error: action.payload.message };
     default:
