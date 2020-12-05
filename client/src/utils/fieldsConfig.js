a686be5f8f1b570de259857ddbf2b8f66f5883bc
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-12-05 11:46:21 -0800

    Refactored by splitting it into 2 functions for properties and errors

--- client/src/utils/fieldsConfig.js
+++ client/src/utils/fieldsConfig.js
@@ -1,70 +1,25 @@
 import React from 'react';
 import FormControl from 'react-bootstrap';
 
-export default apiError => ({ touched, errors, handleChange, handleBlur }) => {
+export const fieldInputProperties = (type, name, placeholder, apiError) => ({
+  touched,
+  errors,
+  handleChange,
+  handleBlur
+}) => {
   return {
-    nameProperty: {
-      type: 'text',
-      name: 'name',
-      placeholder: 'Name',
-      className: touched.name && errors.name ? 'error' : null,
-      onChange: handleChange,
-      onBlur: handleBlur,
-      isInvalid: !!errors.name || !!apiError
-    },
-    emailProperty: {
-      type: 'email',
-      name: 'email',
-      placeholder: 'Email',
-      className: touched.email && errors.email ? 'error' : null,
-      onChange: handleChange,
-      onBlur: handleBlur,
-      isInvalid: !!errors.email || !!apiError
-    },
-    passwordProperty: {
-      type: 'password',
-      name: 'password',
-      placeholder: 'Password',
-      className: touched.password && errors.password ? 'error' : null,
-      onChange: handleChange,
-      onBlur: handleBlur,
-      isInvalid: !!errors.password || !!apiError
-    },
-    passwordConfirmProperty: {
-      type: 'password',
-      name: 'password_confirmation',
-      placeholder: 'Confirm Password',
-      className:
-        touched.password_confirmation && errors.password_confirmation
-          ? 'error'
-          : null,
-      onChange: handleChange,
-      onBlur: handleBlur,
-      isInvalid: !!errors.password_confirmation || !!apiError
-    },
-    nameErrors:
-      touched.name && errors.name ? (
-        <FormControl.Feedback type="invalid">
-          {errors.name}
-        </FormControl.Feedback>
-      ) : null,
-    emailErrors:
-      touched.email && errors.email ? (
-        <FormControl.Feedback type="invalid">
-          {errors.email}
-        </FormControl.Feedback>
-      ) : null,
-    passwordErrors:
-      touched.password && errors.password ? (
-        <FormControl.Feedback type="invalid">
-          {errors.password}
-        </FormControl.Feedback>
-      ) : null,
-    passwordConfirmErrors:
-      touched.password_confirmation && errors.password_confirmation ? (
-        <FormControl.Feedback type="invalid">
-          {errors.password_confirmation}
-        </FormControl.Feedback>
-      ) : null
+    type,
+    name,
+    placeholder,
+    className: touched[name] && errors[name] ? 'error' : null,
+    onChange: handleChange,
+    onBlur: handleBlur,
+    isInvalid: !!errors[name] || !!apiError
   };
+};
+
+export const fieldInputErrors = name => ({ touched, errors }) => {
+  return touched[name] && errors[name] ? (
+    <FormControl.Feedback type="invalid">{errors[name]}</FormControl.Feedback>
+  ) : null;
 };
