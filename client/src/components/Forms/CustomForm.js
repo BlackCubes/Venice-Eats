89143029f229f1392cc1d76f1307500bf498e42b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-12-05 18:36:16 -0800

    Implemented the field HOC

--- client/src/components/Forms/CustomForm.js
+++ client/src/components/Forms/CustomForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-// import { fieldInputProperties, fieldInputErrors } from './../HOC/withField';
+import { fieldInputProperties, fieldInputErrors } from './../HOC/withField';
 
 import FormCustomInputs from './../Inputs/FormCustomInputs';
 
@@ -18,54 +18,54 @@ export default props => {
     inputErrList
   } = props;
 
-  const fieldInputProperties = (
-    type,
-    name,
-    placeholder
-    // apiError
-  ) => {
-    return {
-      type,
-      name,
-      placeholder,
-      className: touched[name] && errors[name] ? 'error' : null,
-      onChange: handleChange,
-      onBlur: handleBlur,
-      isInvalid: !!errors[name]
-    };
-  };
+  // const fieldInputProperties = (
+  //   type,
+  //   name,
+  //   placeholder
+  //   // apiError
+  // ) => {
+  //   return {
+  //     type,
+  //     name,
+  //     placeholder,
+  //     className: touched[name] && errors[name] ? 'error' : null,
+  //     onChange: handleChange,
+  //     onBlur: handleBlur,
+  //     isInvalid: !!errors[name]
+  //   };
+  // };
 
-  const fieldInputErrors = name => {
-    return touched[name] && errors[name] ? (
-      <Form.Control.Feedback type="invalid">
-        {errors[name]}
-      </Form.Control.Feedback>
-    ) : null;
-  };
-
-  const inputProperties = inputPropList.map(prop => {
-    return fieldInputProperties(prop.type, prop.name, prop.placeholder);
-  });
-
-  const inputErrors = inputErrList.map(prop => {
-    return fieldInputErrors(prop.name);
-  });
+  // const fieldInputErrors = name => {
+  //   return touched[name] && errors[name] ? (
+  //     <Form.Control.Feedback type="invalid">
+  //       {errors[name]}
+  //     </Form.Control.Feedback>
+  //   ) : null;
+  // };
 
   // const inputProperties = inputPropList.map(prop => {
-  //   fieldInputProperties(
-  //     prop.type,
-  //     prop.name,
-  //     prop.placeholder,
-  //     touched,
-  //     errors,
-  //     handleChange,
-  //     handleBlur
-  //   );
+  //   return fieldInputProperties(prop.type, prop.name, prop.placeholder);
   // });
 
   // const inputErrors = inputErrList.map(prop => {
-  //   fieldInputErrors(prop.name, touched, errors);
+  //   return fieldInputErrors(prop.name);
   // });
+
+  const inputProperties = inputPropList.map(prop => {
+    return fieldInputProperties(
+      prop.type,
+      prop.name,
+      prop.placeholder,
+      touched,
+      errors,
+      handleChange,
+      handleBlur
+    );
+  });
+
+  const inputErrors = inputErrList.map(prop => {
+    return fieldInputErrors(prop.name, touched, errors);
+  });
 
   return (
     <Form noValidate onSubmit={handleSubmit}>
