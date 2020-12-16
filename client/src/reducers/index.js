5936b79c4d11d8968286ccdccf1f4a089f775496
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-12-16 08:28:09 -0800

    Imported and exported the foodtruck reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
+import foodtruckReducer from './foodtruckReducer';
 import userReducer from './userReducer';
 
 export default combineReducers({
   auth: authReducer,
-  apiUser: userReducer,
-  apiError: errorReducer
+  apiError: errorReducer,
+  apiFoodtruck: foodtruckReducer,
+  apiUser: userReducer
 });
