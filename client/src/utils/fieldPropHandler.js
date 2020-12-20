bb88e14f410fde6a859c5f47fef6e7c72e931440
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-12-20 08:08:17 -0800

    Refactored with the implementation of get in formik utility function

--- client/src/utils/fieldPropHandler.js
+++ client/src/utils/fieldPropHandler.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getIn } from 'formik';
 import FormControl from 'react-bootstrap/FormControl';
 
 export const fieldInputProperties = (
@@ -15,17 +16,24 @@ export const fieldInputProperties = (
   return {
     type,
     name,
-    defaultValue: values[name],
+    // defaultValue: values[name],
+    defaultValue: getIn(values, name),
     placeholder,
-    className: touched[name] && errors[name] ? 'error' : null,
+    // className: touched[name] && errors[name] ? 'error' : null,
+    className: getIn(touched, name) && getIn(errors, name) ? 'error' : null,
     onChange: handleChange,
     onBlur: handleBlur,
-    isInvalid: !!errors[name] || !!apiError
+    // isInvalid: !!errors[name] || !!apiError
+    isInvalid: !!getIn(errors, name) || !!apiError
   };
 };
 
 export const fieldInputErrors = (name, touched, errors) => {
-  return touched[name] && errors[name] ? (
-    <FormControl.Feedback type="invalid">{errors[name]}</FormControl.Feedback>
+  // return touched[name] && errors[name] ? (
+  return getIn(touched, name) && getIn(errors, name) ? (
+    // <FormControl.Feedback type="invalid">{errors[name]}</FormControl.Feedback>
+    <FormControl.Feedback type="invalid">
+      {getIn(errors, name)}
+    </FormControl.Feedback>
   ) : null;
 };
