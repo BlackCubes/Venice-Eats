3d334b4ab205aab0f7d080ca625b50f3d3170bc8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-12-20 08:12:25 -0800

    Refactored

--- client/src/utils/fieldPropHandler.js
+++ client/src/utils/fieldPropHandler.js
@@ -16,22 +16,17 @@ export const fieldInputProperties = (
   return {
     type,
     name,
-    // defaultValue: values[name],
     defaultValue: getIn(values, name),
     placeholder,
-    // className: touched[name] && errors[name] ? 'error' : null,
     className: getIn(touched, name) && getIn(errors, name) ? 'error' : null,
     onChange: handleChange,
     onBlur: handleBlur,
-    // isInvalid: !!errors[name] || !!apiError
     isInvalid: !!getIn(errors, name) || !!apiError
   };
 };
 
 export const fieldInputErrors = (name, touched, errors) => {
-  // return touched[name] && errors[name] ? (
   return getIn(touched, name) && getIn(errors, name) ? (
-    // <FormControl.Feedback type="invalid">{errors[name]}</FormControl.Feedback>
     <FormControl.Feedback type="invalid">
       {getIn(errors, name)}
     </FormControl.Feedback>
