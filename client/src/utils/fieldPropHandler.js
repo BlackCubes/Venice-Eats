4d38a9d68c47ed57ff6a93eb80a0f47cd3177f9c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2020-12-20 10:29:49 -0800

    Refactored the function

--- client/src/utils/fieldPropHandler.js
+++ client/src/utils/fieldPropHandler.js
@@ -2,28 +2,42 @@ import React from 'react';
 import { getIn } from 'formik';
 import FormControl from 'react-bootstrap/FormControl';
 
-export const fieldInputProperties = (
-  type,
-  name,
-  placeholder,
-  apiError,
-  values,
-  touched,
-  errors,
-  handleChange,
-  handleBlur,
-  as
-) => {
+// export const fieldInputProperties = (
+//   type,
+//   name,
+//   placeholder,
+//   apiError,
+//   values,
+//   touched,
+//   errors,
+//   handleChange,
+//   handleBlur,
+//   as
+// ) => {
+//   return {
+//     type,
+//     name,
+//     defaultValue: getIn(values, name),
+//     placeholder,
+//     className: getIn(touched, name) && getIn(errors, name) ? 'error' : null,
+//     onChange: handleChange,
+//     onBlur: handleBlur,
+//     isInvalid: !!getIn(errors, name) || !!apiError,
+//     as
+//   };
+// };
+
+export const fieldInputProperties = (changes, static) => {
   return {
-    type,
-    name,
-    defaultValue: getIn(values, name),
-    placeholder,
-    className: getIn(touched, name) && getIn(errors, name) ? 'error' : null,
-    onChange: handleChange,
-    onBlur: handleBlur,
-    isInvalid: !!getIn(errors, name) || !!apiError,
-    as
+    defaultValue: getIn(changes.values, static.name),
+    className:
+      getIn(changes.touched, static.name) && getIn(changes.errors, static.name)
+        ? 'error'
+        : null,
+    onChange: changes.handleChange,
+    onBlur: changes.handleBlur,
+    isInvalid: !!getIn(changes.errors, static.name) || !!changes.apiError,
+    ...static
   };
 };
 
